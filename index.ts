9cb33a0b614fa7a6a24757ed800dc5f1e68f1090
Bořek Bartolšic <logimage110@seznam.cz>
2021-01-05 13:04:20 +0100

    new TS

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
 import http from 'http';
 import primes from 'primes'
 //var http = require('http');
+type Person = {
+    id: number | string;
+    name: string;
+}
 
-http.createServer(function(req, res) {
+
+const createPersons = () =>{
     const nums = primes(0,100);
+    const presons : Person[] = nums.map((num, index) => {
+        return{
+            id: index,
+            name: "osoba_" + num,
+        }
+    })
+    return presons;
+}
+http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    nums.forEach((num)=>res.write('cilo je ${num} \n'))
+    createPersons().forEach((person)=>res.write(`${person.name} \n`))
     res.end('Test');
 }).listen(8080);
